e07992d7c4ee025489d558a852cadd6c0c23e53e
Jeremy Bywater <6372964+jlbyh2o@users.noreply.github.com>
2022-02-25 16:46:59 -0700

    Updated URLs

--- foundation/urls.py
+++ foundation/urls.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView
 
 # Test patern, renders Zurb Foundation default page using base template
 urlpatterns = [
-    path('/', TemplateView.as_view(template_name="foundation/index.html"),
+    path('', TemplateView.as_view(template_name="foundation/index.html"),
          name="foundation_index"),
 
-    path('icons/', TemplateView.as_view(template_name="foundation/icons.html"),
+    path('icons', TemplateView.as_view(template_name="foundation/icons.html"),
          name="foundation_icons"),
 ]
